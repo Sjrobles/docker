e1931b22dc8bc549a94ab148a3621c6bf66eb2a6
sjrobles <sjrobles@uninorte.edu.co>
2025-02-04 17:25:23 -0500

    commit

---
+++ ejecuciones.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Ejecutar Python
+echo "Entrando a la carpeta Python..."
+cd py
+docker build -t python-image .
+docker run --rm python-image
+cd ..
+
+# Ejecutar JavaScript
+echo "Entrando a la carpeta JavaScript..."
+cd js
+docker build -t js-image .
+docker run --rm js-image
+cd ..
+
+# Ejecutar Java
+echo "Entrando a la carpeta Java..."
+cd java
+docker build -t java-image .
+docker run --rm java-image
+cd ..
+
+# Ejecutar C#
+echo "Entrando a la carpeta C#..."
+cd cs
+docker build -t cs-image .
+docker run --rm cs-image
+cd ..
+
+echo "Todos los scripts ejecutados."
