95483a2c0b7f1de503bd62fd267f1144a4faa08b
Sjrobles <128165072+Sjrobles@users.noreply.github.com>
2025-02-04 14:34:20 -0500

    Create factorial.js

---
+++ js/factorial.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+console.time("Tiempo de ejecución");
+
+function factorial(n) {
+    let result = BigInt(1);
+    for (let i = 2; i <= n; i++) {
+        result *= BigInt(i);
+    }
+    return result;
+}
+
+const n = 10000;
+const result = factorial(n);
+
+console.timeEnd("Tiempo de ejecución");
+
+// Guardar los resultados en un archivo
+fs.appendFileSync('resultadoJS.txt', `Tiempo de ejecución: ${process.hrtime.bigint()} ns\n`, 'utf8');
