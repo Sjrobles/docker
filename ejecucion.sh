6d9e74e09a1647a76c44c490a54eb5747d19c858
Sjrobles <128165072+Sjrobles@users.noreply.github.com>
2025-02-07 14:56:33 -0500

    Update ejecucion.sh

--- ejecucion.sh
+++ ejecucion.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+RESULTS_FILE="benchmark_results.txt"
+echo "Resultados del Benchmark:" > $RESULTS_FILE
+
 # Ejecutar Python
 echo "Entrando a la carpeta Python..."
 cd py
 docker build -t python-image .
 EXEC_TIME_PY=$(docker run --rm python-image | awk '{print $NF}')
+echo "Python: $EXEC_TIME_PY ms" >> ../$RESULTS_FILE
 cd ..
 
 # Ejecutar JavaScript
@@ -12,6 +16,7 @@ echo "Entrando a la carpeta JavaScript..."
 cd js
 docker build -t js-image .
 EXEC_TIME_JS=$(docker run --rm js-image | awk '{print $NF}')
+echo "JavaScript: $EXEC_TIME_JS ms" >> ../$RESULTS_FILE
 cd ..
 
 # Ejecutar Java
@@ -19,6 +24,7 @@ echo "Entrando a la carpeta Java..."
 cd java
 docker build -t java-image .
 EXEC_TIME_JAVA=$(docker run --rm java-image | awk '{print $NF}')
+echo "Java: $EXEC_TIME_JAVA ms" >> ../$RESULTS_FILE
 cd ..
 
 # Ejecutar C#
@@ -26,26 +32,22 @@ echo "Entrando a la carpeta C#..."
 cd cs
 docker build -t cs-image .
 EXEC_TIME_CS=$(docker run --rm cs-image | awk '{print $NF}')
+echo "C#: $EXEC_TIME_CS ms" >> ../$RESULTS_FILE
 cd ..
 
-# Ejecutar C#
+# Ejecutar Ruby
 echo "Entrando a la carpeta Ruby..."
 cd ruby
 docker build -t rb-image .
 EXEC_TIME_RB=$(docker run --rm rb-image | awk '{print $NF}')
+echo "Ruby: $EXEC_TIME_RB ms" >> ../$RESULTS_FILE
 cd ..
 
-# Crear un array con los tiempos de ejecución
-declare -A exec_times
-exec_times["Python"]=$EXEC_TIME_PY
-exec_times["JavaScript"]=$EXEC_TIME_JS
-exec_times["Java"]=$EXEC_TIME_JAVA
-exec_times["C#"]=$EXEC_TIME_CS
-exec_times["Ruby"]=$EXEC_TIME_RB
-
 # Ordenar los tiempos de ejecución de menor a mayor
-for lang in "${!exec_times[@]}"; do
-  echo "$lang: ${exec_times[$lang]} ms"
-done | sort -t: -k2 -n
+echo "Resultados ordenados:" >> $RESULTS_FILE
+sort -t: -k2 -n $RESULTS_FILE -o $RESULTS_FILE
 
-echo "Todos los scripts ejecutados. :)" 
+# Mapear el archivo de resultados a un volumen en el contenedor
+docker run --rm -v $(pwd)/$RESULTS_FILE:/benchmark_results.txt alpine cat /benchmark_results.txt
+
+echo "Todos los scripts ejecutados. Resultados guardados en $RESULTS_FILE :)"
